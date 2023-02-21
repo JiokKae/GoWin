6cae69301be67bf8d360cae651280106f487a214
JiokKae <ekaktm1@gmail.com>
2023-02-22 03:43:47 +0900

    기본 생성자로 대체

--- GoWin/stdgo.h
+++ GoWin/stdgo.h
@@ -11,26 +11,12 @@ char Color2Char( Color color );
 
 struct Coord2d 
 {
-	Coord2d(int x = 0, int y = 0) 
-		: x(x)
-		, y(y)
-	{
-	}
-
 	int x;
 	int y;
 };
 
 struct PlacementInfo
 {
-	PlacementInfo(int x, int y, int sequence, Color player)
-		: x(x)
-		, y(y)
-		, sequence(sequence)
-		, player(player)
-	{
-	}
-
 	int x, y;
 	int sequence;
 	Color player;
